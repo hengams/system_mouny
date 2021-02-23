95c4a22823ed3d9e0cf2b11a4fe9dec8b2f1a962
soroosh <sorooshhengam@gmail.com>
2021-02-23 10:43:09 +0330

    add main file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Block
+{
+public:
+    Block(string name, int credit)
+    {
+
+    }
+
+private:
+    string name;
+    int credit;
+};
+
+int main()
+{
+    cout << "hi" << endl;
+    return 0;
+}
